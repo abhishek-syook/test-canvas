f095174169af3955120bc03e7598c7c0d27d412b
abhishek <abhishek@syook.com>
2022-03-29 13:44:19 +0530

    add grid tool to show hide grid in canvas

---
+++ src/utils/drawGrid.js
@@ -0,0 +1,31 @@
+const drawGrid = ({ context, isEnable, snapSize }) => {
+  if (!context || !isEnable || snapSize === 0) return;
+
+  //grid width and height
+  const bw = snapSize;
+  const bh = snapSize;
+
+  //padding around grid
+  // const p = 10;
+
+  //size of canvas
+  let cw = context.canvas.clientWidth;
+  let ch = context.canvas.clientHeight;
+
+  context.beginPath();
+  for (let x = 0; x <= cw; x += bw) {
+    context.moveTo(x, 0);
+    context.lineTo(x, ch);
+  }
+
+  for (let y = 0; y <= ch; y += bh) {
+    context.moveTo(0, y);
+    context.lineTo(cw, y);
+  }
+
+  context.strokeStyle = "black";
+  context.stroke();
+  context.closePath();
+};
+
+export default drawGrid;
