134a7395843df4ed6eb8627f44ceb1860422c90b
Harshith <harshith@syook.com>
2022-04-04 10:24:40 +0530

    move all mouse pointer related helpers to seperate file

--- src/components/canvasDrawing/index.jsx
+++ src/components/canvasDrawing/index.jsx
@@ -3,21 +3,20 @@ import PropTypes from 'prop-types';
 import HistoryToolbar from 'components/toolbar/historyToolbar';
 import useHistory from 'hooks/useHistory';
 
+import { adjustmentRequired, drawElement } from 'utils/roughHelper';
 import {
 	adjustElementCoordinates,
-	adjustmentRequired,
 	createElement,
 	cursorForPosition,
-	drawElement,
 	getElementAtPosition,
-	getRadius,
 	nearPoint,
+	getRadius,
 	resizedCoordinates,
 	diffPoints,
 	addPoints,
 	scalePoint,
 	updatedPoints
-} from 'utils/roughHelper';
+} from 'utils/mouseHelper';
 import {
 	ACTIONS,
 	CURSOR,
