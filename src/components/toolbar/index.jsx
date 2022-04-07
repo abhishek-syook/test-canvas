0a6a2ceaaf0167c86e64ef8e3264d0e5be3c000e
Harshith <harshith@syook.com>
2022-04-07 10:27:24 +0530

    remove all icon components from toolIcons folder, create toolIcons file to utilize all icons and remove ELEMENT_TYPES_DATA from constants

--- src/components/toolbar/index.jsx
+++ src/components/toolbar/index.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ELEMENT_TYPES_DATA } from '../../constants';
+import { ELEMENT_TYPES } from '../../constants';
+import ToolIcons from 'assets/toolIcons';
 
 const Toolbar = ({ selectedTool, onChange }) => {
 	return (
 		<div style={{ position: 'fixed', display: 'flex' }}>
-			{ELEMENT_TYPES_DATA.map(element => {
+			{Object.values(ELEMENT_TYPES).map(element => {
 				return (
-					<div key={element.type} title={`${element.label}`}>
+					<div key={element} title={element[0].toUpperCase() + element.slice(1)}>
 						<button
-							onClick={() => onChange(element.type)}
-							className="toolBar"
+							onClick={() => onChange(element)}
 							style={
 								selectedTool === element.type
-									? { backgroundColor: '#F5F4FF', color: '#4724D8', width: '20', height: '20' }
-									: { backgroundColor: '#FDFDFD', width: '20', height: '20' }
+									? { backgroundColor: '#F5F4FF', color: '#4724D8' }
+									: { backgroundColor: '#FDFDFD' }
 							}
 						>
-							{element.icon}
+							<ToolIcons name={element} />
 						</button>
 					</div>
 				);
